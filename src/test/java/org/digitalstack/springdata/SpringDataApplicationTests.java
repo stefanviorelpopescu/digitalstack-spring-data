1de8e610bc887d98fa0d40f4678d292d643f8d6c
Stefan <stefan@vestinda.com>
2023-09-20 09:19:19 +0300

    Initial commit.

---
+++ src/test/java/org/digitalstack/springdata/SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.digitalstack.springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
